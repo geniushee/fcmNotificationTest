673299483452bf1e1f8d0ac69733cd370fa140ae
geniushee <h1116jeon@gmail.com>
2025-05-23 16:38:07 +0900

    파이어베이스 앱 초기화

---
+++ src/main/java/com/example/fcm/config/FirebaseConfig.java
@@ -0,0 +1,25 @@
+package com.example.fcm.config;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import jakarta.annotation.PostConstruct;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+@Configuration
+public class FirebaseConfig {
+    @PostConstruct
+    public void init() throws IOException{
+        FileInputStream serviceAccount =
+                new FileInputStream("src/main/resources/fir-testproject-5dd98-firebase-adminsdk-fbsvc-c7f40c68fb.json");
+
+        FirebaseOptions options = new FirebaseOptions.Builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .build();
+
+        FirebaseApp.initializeApp(options);
+    }
+}
