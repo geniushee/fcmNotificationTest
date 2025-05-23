3ab43530b0a7ecdc3c20fb70c4dc865f059b9bf6
geniushee <h1116jeon@gmail.com>
2025-05-23 16:42:06 +0900

    FCMService 생성

--- src/main/java/com/example/fcm/fcm/FCMService.java
+++ src/main/java/com/example/fcm/fcm/FCMService.java
@@ -1,7 +1,22 @@
 package com.example.fcm.fcm;
 
+import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.firebase.messaging.FirebaseMessagingException;
+import com.google.firebase.messaging.Message;
+import com.google.firebase.messaging.Notification;
 import org.springframework.stereotype.Service;
 
 @Service
 public class FCMService {
+    public void sendMessage(String targetToken, String title, String body) throws FirebaseMessagingException {
+        Message message = Message.builder()
+                .setToken(targetToken)
+                .setNotification(Notification.builder()
+                        .setTitle(title)
+                        .setBody(body)
+                        .build())
+                .build();
+
+        FirebaseMessaging.getInstance().send(message);
+    }
 }
